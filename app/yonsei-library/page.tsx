f1a8efbc6633a6b208dbdb91189a99dffec41b34
jhyunwoo <jhyunwoo0228@gmail.com>
2025-09-22 16:31:13 +0900

    add loading ui

--- app/yonsei-library/page.tsx
+++ app/yonsei-library/page.tsx
@@ -9,7 +9,7 @@ export default function YLBSPage() {
         "w-full min-h-screen flex items-center justify-center flex-col"
       }
     >
-      <div className={"w-full h-[20vh]"} />
+      <div className={"w-full h-[10vh]"} />
       <div
         className={
           "w-full p-4 bg-neutral-50 rounded-xl max-w-4xl flex flex-col gap-2 min-h-1/3"
@@ -18,7 +18,15 @@ export default function YLBSPage() {
         <h1 className={"text-2xl font-bold"}>도서관 세미나룸 예약</h1>
         <BookLibraryRoomForm />
       </div>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense
+        fallback={
+          <div
+            className={
+              "w-full rounded-xl bg-neutral-300 animate-pulse h-16 max-w-4xl mt-2"
+            }
+          />
+        }
+      >
         <BookingHistory />
       </Suspense>
     </div>
